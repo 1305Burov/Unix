cf816c421403c0d4e0f949b7270bae13e18be5c0
1305Burov <1305burov@gmail.com>
2022-03-29 14:49:04 +0300

    header menu added

--- src/js/app.js
+++ src/js/app.js
@@ -3,3 +3,8 @@ import * as flsFunc from './moduls/func.js';
 flsFunc.isWebp();
 
 import Swiper, { Navigation, Pagination } from 'swiper';
+
+
+document.querySelector('.header__dropdown').addEventListener('click', () => {
+    document.querySelector('.header__dropdown').classList.toggle('header__dropdown_open')
+})
