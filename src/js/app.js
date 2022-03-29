0c1dc0ed601f5106737bf82dbd69b33d071e4040
1305Burov <1305burov@gmail.com>
2022-03-30 00:12:31 +0300

    buttons animation added

--- src/js/app.js
+++ src/js/app.js
@@ -8,3 +8,24 @@ import Swiper, { Navigation, Pagination } from 'swiper';
 document.querySelector('.header__dropdown').addEventListener('click', () => {
     document.querySelector('.header__dropdown').classList.toggle('header__dropdown_open')
 })
+
+document.querySelectorAll('.button-cta').forEach(item => {
+    item.addEventListener('click', animateClick);
+})
+
+function animateClick(e) {
+    let animationCircle = document.createElement('div'),
+        mVal = Math.max(this.clientWidth, this.clientHeight),
+        Rect = e.target.getBoundingClientRect();
+
+        animationCircle.style.width = `${mVal}px`;
+        animationCircle.style.height = `${mVal}px`;
+        animationCircle.style.left = `${e.clientX - Rect.left - (mVal / 2)}px`;
+        animationCircle.style.top = `${e.clientY - Rect.top - (mVal / 2)}px`;
+
+        animationCircle.classList.add('click-animation');
+        this.appendChild(animationCircle);
+        setTimeout(() => {
+            animationCircle.remove();
+        }, 500);
+}
