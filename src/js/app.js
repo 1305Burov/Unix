021110fe02f30e94ca4345a461fbbe27a4b1f4be
1305Burov <1305burov@gmail.com>
2022-04-02 13:52:01 +0300

    banner and partners added

--- src/js/app.js
+++ src/js/app.js
@@ -4,6 +4,8 @@ flsFunc.isWebp();
 
 import Swiper, { Navigation, Pagination } from 'swiper';
 
+new Swiper('.swiper');
+
 
 document.querySelector('.header__dropdown').addEventListener('click', () => {
     document.querySelector('.header__dropdown').classList.toggle('header__dropdown_open')
