6ba2ac94a331c3cfabf89a4f1b221c62df1b08a4
1305Burov <1305burov@gmail.com>
2022-04-03 18:05:44 +0300

    section stock and adv added

--- src/js/app.js
+++ src/js/app.js
@@ -2,10 +2,18 @@ import * as flsFunc from './moduls/func.js';
 
 flsFunc.isWebp();
 
-import Swiper, { Navigation, Pagination } from 'swiper';
+// import Swiper, { Navigation, Pagination } from 'swiper';
 
-new Swiper('.swiper');
-
+new Swiper('.swiper', {
+    // modules: [Navigation, Pagination],
+    loop: true,
+    slidesPerView: 7,
+    initialSlide: 3,
+    centeredSlides: true,
+    autoplay: {
+        delay: 2500,
+    },
+});
 
 document.querySelector('.header__dropdown').addEventListener('click', () => {
     document.querySelector('.header__dropdown').classList.toggle('header__dropdown_open')
@@ -20,15 +28,14 @@ function animateClick(e) {
         mVal = Math.max(this.clientWidth, this.clientHeight),
         Rect = e.target.getBoundingClientRect();
 
-        animationCircle.style.width = `${mVal}px`;
-        animationCircle.style.height = `${mVal}px`;
-        animationCircle.style.left = `${e.clientX - Rect.left - (mVal / 2)}px`;
-        animationCircle.style.top = `${e.clientY - Rect.top - (mVal / 2)}px`;
+    animationCircle.style.width = `${mVal}px`;
+    animationCircle.style.height = `${mVal}px`;
+    animationCircle.style.left = `${e.clientX - Rect.left - (mVal / 2)}px`;
+    animationCircle.style.top = `${e.clientY - Rect.top - (mVal / 2)}px`;
 
-        animationCircle.classList.add('click-animation');
-        this.appendChild(animationCircle);
-        setTimeout(() => {
-            animationCircle.remove();
-        }, 500);
+    animationCircle.classList.add('click-animation');
+    this.appendChild(animationCircle);
+    setTimeout(() => {
+        animationCircle.remove();
+    }, 500);
 }
-
